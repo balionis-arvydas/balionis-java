5f884a83d08a227f99599ed366f05b5ceeb7fd44
baliarvy <arvydas@balionis.com>
2018-05-03 18:06:09 +0100

    JIRA-0000: added unittests

---
+++ balionis-paint/src/test/java/com/balionis/paint/PaintControllerTest.java
@@ -0,0 +1,127 @@
+package com.balionis.paint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import org.mockito.Mockito;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import com.balionis.paint.action.*;
+import com.balionis.paint.console.*;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = PaintControllerTest.Config.class)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class PaintControllerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(PaintControllerTest.class);
+
+    public static class TestConsoleWriter extends ConsoleWriter {
+
+        private List<String> lines = new ArrayList<>();
+        private StringBuilder builder = new StringBuilder();
+
+        public TestConsoleWriter() {
+            super();
+        }
+
+        @Override
+        public void writeLine(String line) {
+            lines.add(builder.toString() + line);
+            builder = new StringBuilder();
+        }
+
+        @Override
+        public void write(String line) {
+            builder.append(line);
+        }
+
+        public List<String> getLines() {
+            return lines;
+        }
+    }
+
+    @Autowired
+    private ConsoleWriter writer;
+
+    @Autowired
+    private PaintController controller;
+
+    @Test
+    public void testPaint() throws ActionException {
+
+        String[] args = {"C", "20", "5"};
+        Action prototypeForCreate = new ActionForCreate();
+        Action create = prototypeForCreate.builder().withArguments(args).build();
+
+        create.handle(controller);
+
+        String[] expect1 =  {
+                "----------------------",
+                "|                    |",
+                "|                    |",
+                "|                    |",
+                "|                    |",
+                "|                    |",
+                "----------------------" };
+
+        String[] actual1 = controller.getCanvas().getLines().toArray(new String[0]);
+        logger.debug("testPaint: actual1={}", Arrays.toString(actual1));
+
+        assertTrue(Arrays.equals(expect1, actual1));
+
+        String[] expect2 =  {
+                "", // always print blank line as first
+                "----------------------",
+                "|                    |",
+                "|                    |",
+                "|                    |",
+                "|                    |",
+                "|                    |",
+                "----------------------" };
+        String[] actual2 = ((TestConsoleWriter) writer).getLines().toArray(new String[0]);
+
+        logger.debug("testPaint: actual2={}", Arrays.toString(actual2));
+        assertTrue(Arrays.equals(expect2, actual2));
+
+    }
+
+    @Configuration
+    static class Config {
+
+        @Bean
+        public PaintController getController() {
+            return new PaintController();
+        }
+
+        @Bean
+        public CanvasBuilder getCanvasBuilder() {
+            return new CanvasBuilder();
+        }
+
+        @Bean
+        public ConsoleWriter getConsoleWriter() {
+            return new TestConsoleWriter();
+        }
+
+    }
+}
--- balionis-paint/src/test/java/com/balionis/paint/PaintRunnerTest.java
+++ balionis-paint/src/test/java/com/balionis/paint/PaintRunnerTest.java
@@ -2,6 +2,9 @@ package com.balionis.paint;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
 import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +13,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 import com.balionis.paint.action.*;
 import com.balionis.paint.console.*;
+
+import java.util.Arrays;
 
 /**
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = PaintRunnerTest.Config.class)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class PaintRunnerTest {
 
     @MockBean
-    private ActionReader readerMock;
+    private ConsoleReader readerMock;
 
     @MockBean
     private ConsoleWriter writerMock;
@@ -35,18 +41,79 @@ public class PaintRunnerTest {
     private PaintRunner runner;
 
     @Test
-    public void testMe() throws ActionException {
-        Action prototype = new ActionForQuit();
-        Action action = prototype.builder().withArguments(Arrays.asList("Q").toArray(new String[0])).withStopper(runner).build();
+    public void testQuit() {
 
-        Mockito.doReturn(action).when(readerMock).readNext(runner);
+        Mockito.reset(readerMock);
+        Mockito.when(readerMock.readLine()).thenReturn("Q");
 
         Mockito.doNothing().when(writerMock).write(Mockito.anyString());
         Mockito.doNothing().when(writerMock).writeLine(Mockito.anyString());
 
         runner.run();
 
-        Mockito.verify(readerMock, Mockito.times(1)).readNext(runner);
+        Mockito.verify(readerMock, Mockito.times(1)).readLine();
+    }
+
+    @Test
+    public void testCreate() {
+
+        Mockito.reset(readerMock);
+        Mockito.when(readerMock.readLine())
+                .thenReturn("C 6 4")
+                .thenReturn("Q")
+                .thenReturn("X");
+
+        Mockito.doNothing().when(writerMock).write(Mockito.anyString());
+        Mockito.doNothing().when(writerMock).writeLine(Mockito.anyString());
+
+        String[] expect =  {
+                "--------",
+                "|      |",
+                "|      |",
+                "|      |",
+                "|      |",
+                "--------" };
+
+        runner.run();
+
+        String[] actual = controller.getCanvas().getLines().toArray(new String[0]);
+
+        assertTrue(Arrays.equals(expect, actual));
+
+        Mockito.verify(readerMock, Mockito.times(2)).readLine();
+    }
+
+    @Test
+    public void testDraw() {
+
+        Mockito.reset(readerMock);
+        Mockito.when(readerMock.readLine())
+                .thenReturn("C 20 5")
+                .thenReturn("L 1 3 7 3")
+                .thenReturn("L 7 1 7 3")
+                .thenReturn("R 15 2 20 5")
+                .thenReturn("Q")
+                .thenReturn("X");
+
+        Mockito.doNothing().when(writerMock).write(Mockito.anyString());
+        Mockito.doNothing().when(writerMock).writeLine(Mockito.anyString());
+
+        String[] expect =  {
+                "----------------------",
+                "|      x             |",
+                "|      x       xxxxxx|",
+                "|xxxxxxx       x    x|",
+                "|              x    x|",
+                "|              xxxxxx|",
+                "----------------------" };
+
+        runner.run();
+
+        String[] actual = controller.getCanvas().getLines().toArray(new String[0]);
+
+        assertTrue(Arrays.equals(expect, actual));
+
+        Mockito.verify(readerMock, Mockito.times(5)).readLine();
     }
 
     @Configuration
@@ -60,6 +127,41 @@ public class PaintRunnerTest {
         @Bean
         public CanvasBuilder getCanvasBuilder() {
             return new CanvasBuilder();
+        }
+
+        @Bean
+        public ActionForCreate getActionForCreate() {
+            return new ActionForCreate();
+        }
+
+        @Bean
+        public ActionForLine getActionForLine() {
+            return new ActionForLine();
+        }
+
+        @Bean
+        public ActionForPaint getActionForPaint() {
+            return new ActionForPaint();
+        }
+
+        @Bean
+        public ActionForQuit getActionForQuit() {
+            return new ActionForQuit();
+        }
+
+        @Bean
+        public ActionForRect getActionForRect() {
+            return new ActionForRect();
+        }
+
+        @Bean
+        public ActionFactory getActionFactory() {
+            return new ActionFactory();
+        }
+
+        @Bean
+        public ActionReader getActionReader() {
+            return new ActionReader();
         }
 
         @Bean
