58fbf290f170a76d8bf1001edc58cf00165fdd5c
balionis-arvydas <arvydas@balionis.com>
2018-08-03 12:35:10 +0100

    JIRA-0000: remove explicit app jar from classpath

--- balionis-docker2/src/main/scripts/app.py
+++ balionis-docker2/src/main/scripts/app.py
@@ -36,7 +36,7 @@ def main_args():
     return args
 
 def main(args):
-    jars = [os.path.join('lib','balionis-docker2-1.0-SNAPSHOT.jar')]
+    jars = []
     for file in os.listdir('lib'):
         if file.endswith('.jar'):
             jars.append(os.path.join('lib', str(file)))
