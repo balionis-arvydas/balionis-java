9900d80744739ab7c5f2a22f71148d619a2f6d50
unknown <arvydas@balionis.com>
2019-12-18 11:32:40 +0000

    JIRA-0000: added snow algo

--- balionis-java5/README.md
+++ balionis-java5/README.md
@@ -4,7 +4,7 @@ It's a 'GS' java algos project using gradle.
 1) MyApp1 - what is the least number of ways to get from number n to 1, using operations (divide by) /2, /3, and -1.
 2) MyApp2 - invert a string e.g. "cat".
 3) MyApp3 - detect a cycle in a linked list.
-4) MyApp4 - detect a cycle in a linked list.
+4) MyApp4 - compute snow between hills.
 5) MyApp5 - for n random positive and negative numbers in an array,
             how does one pick a set of numbers that give the largest sum,
             subject to the following constraints: cannot pick numbers which are adjacent in position in the array.
--- balionis-java5/src/main/java/com/balionis/java5/MyApp4.java
+++ balionis-java5/src/main/java/com/balionis/java5/MyApp4.java
@@ -1,51 +1,87 @@
 package com.balionis.java5;
 
-// detect a cycle in a linked list
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+// compute snow among hills
 public class MyApp4 {
-
-    static class MyEntry<T> {
-        T value;
-        MyEntry<T> next;
-
-        public MyEntry(T value, MyEntry<T> next) {
-            this.value = value;
-            this.next = next;
-        }
-    }
 
     public static void main(String[] args) {
 
-        MyEntry<String> good =
-                new MyEntry<>("1",
-                new MyEntry<>("2",
-                new MyEntry<>("3", null)));
+        int[] hills1 = {1, 2, 1, 3, 0, 2, 0};
+        System.out.println("main: test1(3)=" + test(hills1));
 
-        System.out.println("main: test(true)=" + test(good));
+        int[] hills2 = {1, 2, 1};
+        System.out.println("main: test2(0)=" + test(hills2));
 
-        MyEntry<String> last = new MyEntry<>("3", null);
-        MyEntry<String> bad =
-                new MyEntry<>("1",
-                new MyEntry<>("2", last));
-        last.next = bad;
+        int[] hills3 = {3, 2, 1};
+        System.out.println("main: test3(0)=" + test(hills3));
 
-        System.out.println("main: test(false)=" + test(bad));
+        int[] hills4 = {3, 1, 2};
+        System.out.println("main: test4(1)=" + test(hills4));
 
         System.out.println("main: done");
     }
 
-    static boolean test(MyEntry<String> line) {
-        MyEntry<String> slow = line;
-        MyEntry<String> fast = line.next;
-        while(true) {
-            if (slow == null || fast == null || slow.next == null || fast.next == null) {
-                return true;
-            }
-            if (slow.next == fast.next) {
-                return false;
-            }
-            slow = slow.next;
-            fast = fast.next != null ? fast.next.next : null;
+    static long test(int[] hills) {
+
+        if (hills.length < 3) {
+            return 0;
         }
+
+        List<Integer> hillList = Arrays.stream(hills).mapToObj((x) -> new Integer(x)).collect(Collectors.toList());
+
+        List<List<Character>> landscape = build(hillList);
+
+        List<List<Character>> melted = melt(landscape);
+
+        return count(melted);
+    }
+
+    static List<List<Character>> build(List<Integer> hills) {
+        int max = hills.stream().max((a, b) -> a - b).get();
+
+        return hills.stream().map((hill) -> {
+            Stream<Character> snow =
+                    IntStream.range(0, max - hill)
+                             .mapToObj((x) -> new Character('*'));
+            Stream<Character> stone =
+                    IntStream.range(0, hill)
+                            .mapToObj((x) -> new Character('x'));
+
+            return Stream.concat(snow, stone).collect(Collectors.toList());
+        }).collect(Collectors.toList());
+    }
+
+    static List<List<Character>> melt(List<List<Character>> landscape) {
+        int x = landscape.size();
+        int y = landscape.get(0).size();
+        for (int j=0; j < y; j++) {
+            for (int i=0; i < x; i++) {
+                if (landscape.get(i).get(j) == '*') {
+                    landscape.get(i).set(j, ' ');
+                } else {
+                    break;
+                }
+            }
+            for (int i=x-1; i >=0; i--) {
+                if (landscape.get(i).get(j) == '*') {
+                    landscape.get(i).set(j, ' ');
+                } else {
+                    break;
+                }
+            }
+        }
+        return landscape;
+    }
+
+    static long count(List<List<Character>> landscape) {
+        return landscape.stream().flatMap((x) -> x.stream()).filter((x) -> x == '*').count();
     }
 }
 
